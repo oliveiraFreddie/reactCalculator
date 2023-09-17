9e71e72c7504b4b0bae4e93e04973b5ffa0564a5
oliveiraFreddie <freddie.oliveira95@gmail.com>
2023-09-17 10:24:40 +0100

    Add new readme

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Calculator from './main/Calculator';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Calculator from "./main/Calculator";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div>
-    <h1>Calculadora</h1>
+  <>
+    <div>
+      <h1>Calculadora</h1>
       <Calculator />
-  </div>
+    </div>
+    <br></br>
+    <footer className="footer">
+      <span>
+        Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+        <strong>
+          {" "}
+          Fr<span className="text-danger">3</span>d
+        </strong>
+      </span>
+    </footer>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
